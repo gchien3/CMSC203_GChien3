1bb396ffbb6e0bf8e6a9f3f0729ae5206ef3526c
gchien3 <gchien3@montgomerycollege.edu>
2025-09-09 22:09:26 -0400

    Add files via upload

--- GChien_Project1.java
+++ GChien_Project1.java
@@ -1,9 +1,6 @@
 
 public class GChien_Project1 {
 	public static void main(String[] args) {
-		System.out.println("Hello Team");
-		System.out.println("Hello Again");
+		System.out.println("Hello There");
 	}
 }
-
-
